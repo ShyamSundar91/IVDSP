01d59e05e7b8d9d9d95b6103e0060b9f7a07614f
Shyam Sundar <shyam@qampo.com>
2020-06-10 09:52:42 +0200

    Before testing greedy all

--- src/main/java/ALNS/LocalSearch.java
+++ src/main/java/ALNS/LocalSearch.java
@@ -146,8 +146,8 @@ public class LocalSearch
 		this.maxIterations = 0; 
 		this.segmentSize = 25; 
 		
-		int numberOfDestroyMethods = 2;
-		int numberOfRepairMethods = 2; 
+		int numberOfDestroyMethods = 3;
+		int numberOfRepairMethods = 3; 
 		
 		double initialProbabilities = 1.0/(double)(numberOfDestroyMethods); 
 		this.destroyWeights = new double[numberOfDestroyMethods]; 
